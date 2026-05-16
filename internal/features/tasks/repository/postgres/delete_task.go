7b38323da21697f2fa094067bb880a64120d30e7
DarkLead <nemgre@mail.ru>
2026-05-16 09:11:45 +0300

    Add feature tasks

---
+++ internal/features/tasks/repository/postgres/delete_task.go
@@ -0,0 +1,36 @@
+package task_postgres_repository
+
+import (
+	"context"
+	"fmt"
+
+	core_errors "github.com/Sergey-tech9087/petProjectToDoList/internal/core/errors"
+)
+
+func (r *TasksRepository) DeleteTask(
+	ctx context.Context,
+	id int,
+) error {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+	DELETE FROM todo.tasks
+	WHERE id=$1
+	`
+
+	cmdTag, err := r.pool.Exec(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("exec query^ %w", err)
+	}
+
+	if cmdTag.RowsAffected() == 0 {
+		return fmt.Errorf(
+			"task with id='%d': %w",
+			id,
+			core_errors.ErrNotFound,
+		)
+	}
+
+	return nil
+}
